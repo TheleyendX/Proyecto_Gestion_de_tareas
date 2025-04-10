ef2229284a7333fa53f9bf2b80ecd7dae8363c34
Melito <premiumm9@gmail.com>
2025-04-09 18:06:45 -0700

    cambios

---
+++ models/lista.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Lista extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Lista.init({
+    id_lista: DataTypes.INTEGER,
+    nombre: DataTypes.STRING,
+    descripcion: DataTypes.STRING,
+    id_usuario: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Lista',
+  });
+  return Lista;
+};
