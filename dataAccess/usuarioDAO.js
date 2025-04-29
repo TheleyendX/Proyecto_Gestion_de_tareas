68265574bd50e391d95538fa2c58ca48d5e08e80
Melito <premiumm9@gmail.com>
2025-04-28 18:07:44 -0700

    update

--- dataAccess/usuarioDAO.js
+++ dataAccess/usuarioDAO.js
@@ -11,6 +11,33 @@ class UsuarioDAO {
         } catch (error) {
             console.error('Error al crear usuario', error);
             throw error;
+        }        
+    }
+    static async eliminarUsuario(){
+        try {
+            const [result] = await sequelize.query('Call SP_EliminarUsuario');
+            return result;
+        } catch (error) {
+            console.error('Error al crear usuario', error);
+            throw error;
+        }
+    }
+    static async actualizarUsuario(id,nombre, correo, contraseña){
+        try {
+            const [result] = await sequelize.query('Call SP_ModificarUsuario(?,?,?,?)', {replacements: [id, correo, contraseña]});
+            return result;
+        } catch (error) {
+            console.error('Error al crear usuario', error);
+            throw error;
+        }
+    }
+    static async consultarUsuario(){
+        try {
+            const [result] = await sequelize.query('Call SP_ConsultarUsuario');
+            return result;
+        } catch (error) {
+            console.error('Error al crear usuario', error);
+            throw error;
         }
     }
 }
