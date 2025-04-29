3cbd6ddce0558666e2209bbb99d2ac5745b8b587
Melito <premiumm9@gmail.com>
2025-04-28 18:19:02 -0700

    update

--- models/tarea.js
+++ models/tarea.js
@@ -8,7 +8,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      //Definir las relaciones de tarea
+      //Relacion uno a uno de tarea con lista
+      Tarea.hasOne(models.Lista, { foreignKey: 'id_usuario'});
+      models.Lista.belongsTo(Tarea, {foreignKey: 'id_usuario'});
+
     }
   }
   Tarea.init(
