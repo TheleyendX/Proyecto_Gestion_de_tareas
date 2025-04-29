e331db5372b08725073ff2247baa34634af66085
Melito <premiumm9@gmail.com>
2025-04-28 18:26:43 -0700

    update

--- models/tarea.js
+++ models/tarea.js
@@ -12,7 +12,9 @@ module.exports = (sequelize, DataTypes) => {
       //Relacion uno a uno de tarea con lista
       Tarea.hasOne(models.Lista, { foreignKey: 'id_usuario'});
       models.Lista.belongsTo(Tarea, {foreignKey: 'id_usuario'});
-
+      //Relacion uno a uno de tarea a recordatorio
+      Tarea.hasOne(models.Recordatorio, {foreignKey: 'id_usuario'});
+      models.Recordatorio.belongsTo(Tarea, {foreignKey: 'id_usuario'});
 
     }
   }
