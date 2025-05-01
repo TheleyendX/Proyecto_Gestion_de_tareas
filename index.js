26a97598f9c30763a8a95848408be5f0c6833f34
Melito <premiumm9@gmail.com>
2025-04-30 17:28:58 -0700

    Update
    Correcion de migraciones y pruebas de conexion en index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-import config from "./config/dbConnect.js";
+const UsuarioDAO = require('./dataAccess/usuarioDAO');
+const { sequelize } = require('./models');
 
-async function prueba() {
-    await config.authenticate();
-    console.log('BD conectada');
+//Funcion asincrona para ejecutar las operaciones
+ async function ejecutarTransación() {
+    //Realiza la sincronización de los modelos con la BD
+    await sequelize.sync();
+    
 
-    await config.close();
-    console.log('Bd desconectada');  
+    //Cierra la conexion a la BD cuando termine todas las operaciones
+    await sequelize.close();
+ }
 
-}
+ //Realiza las operaciones
+ejecutarTransación();
